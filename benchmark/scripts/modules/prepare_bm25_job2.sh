25f3e63b828ed3803e6ce230955557381fc040f9
haok1402 <haok1402@gmail.com>
2025-03-30 19:28:30 -0400

    okay, certain nodes are just incredibly slow... let's try to reschedule that

--- benchmark/scripts/modules/prepare_bm25_job2.sh
+++ benchmark/scripts/modules/prepare_bm25_job2.sh
@@ -6,8 +6,9 @@
 #SBATCH --partition=general
 
 #SBATCH --array=0-3
-#SBATCH --ntasks=2
-#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=32G
 
 srun -W 0 benchmark/scripts/modules/prepare_bm25_job2_step1.sh
