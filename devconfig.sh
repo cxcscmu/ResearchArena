bf5f15b8fb11fd491ab9f5bff33128c7618f84b4
haok1402 <haok1402@gmail.com>
2025-03-30 15:53:42 -0400

    move things around under a different SSD_MOUNTing path... too large source text

--- devconfig.sh
+++ devconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export NFS_MOUNT=/home/haok/ResearchArena
-export SSD_MOUNT=/data/user_data/haok/ResearchArena
+export SSD_MOUNT=/data/group_data/cx_group/ResearchArena
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ResearchArena
