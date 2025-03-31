286ee9ea5bcc511c7f1f7615516763ded8efcea0
haok1402 <haok1402@gmail.com>
2025-03-31 14:52:32 -0400

    fix the bug transferring from openai to claude

--- benchmark/sources/retrieve_decomposer_claude.py
+++ benchmark/sources/retrieve_decomposer_claude.py
@@ -90,6 +90,7 @@ async def generate_response(session: Session, prompt: str):
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--surveys-file", type=str, required=True)
+    parser.add_argument("--replays-file", type=str, required=True)
     parser.add_argument("--queries-file", type=str, required=True)
     parser.add_argument("--records-file", type=str, required=True)
     parsed = parser.parse_args()
@@ -97,6 +98,9 @@ async def main():
     surveys_file = Path(parsed.surveys_file)
     if surveys_file.suffix != ".jsonl":
         raise NotImplementedError("Only 'jsonl' files are read.")
+    replays_file = Path(parsed.replays_file)
+    if replays_file.suffix != ".jsonl":
+        raise NotImplementedError("Only 'jsonl' files are written.")
     queries_file = Path(parsed.queries_file)
     if queries_file.suffix != ".jsonl":
         raise NotImplementedError("Only 'jsonl' files are written.")
@@ -125,16 +129,28 @@ async def main():
 
     logging.info(f"Writing the queries to {queries_file}.")
     batch_size, session = 32, Session()
-    with queries_file.open("w") as fp:
+    with queries_file.open("w") as fp1, replays_file.open("w") as fp2:
         for i in range(0, len(ids), batch_size):
             batch_ids = ids[i:i + batch_size]
             batch_titles = titles[i:i + batch_size]
             batch_prompts = [prompt_template.format(title=title) for title in batch_titles]
             logging.info(f"Processing batch {i}-{i+batch_size} out of {len(ids)} records...")
             batch_queries = await asyncio.gather(*[generate_response(session, prompt) for prompt in batch_prompts])
-            for id, query in zip(batch_ids, batch_queries):
-                json.dump({"id": id, "query": query}, fp)
-                fp.write("\n")
+            for id, queries in zip(batch_ids, batch_queries):
+                json.dump({"id": id, "replay": queries}, fp2)
+                fp2.write("\n")
+                matched = queries.partition("<Queries>")[-1].strip()
+                if matched == "":
+                    continue
+                matched = matched.partition("</Queries>")[0].strip()
+                if matched == "":
+                    continue
+                for line in matched.splitlines():
+                    query = line.lstrip("- ")
+                    if query == "":
+                        continue
+                    json.dump({"id": id, "query": query}, fp1)
+                    fp1.write("\n")
 
 if __name__ == "__main__":
     asyncio.run(main())
