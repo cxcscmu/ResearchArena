144f78e2e4baab72b85bbe268eb14cdd58f02b71
haok1402 <haok1402@gmail.com>
2025-03-31 07:51:04 -0400

    clean up the task on the fly

--- devconfig.sh
+++ devconfig.sh
@@ -2,6 +2,7 @@
 
 export NFS_MOUNT=/home/haok/ResearchArena
 export SSD_MOUNT=/data/group_data/cx_group/ResearchArena
+find $NFS_MOUNT/queue -type f -name "*.task" -empty -delete
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ResearchArena
